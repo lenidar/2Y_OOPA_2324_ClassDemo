927d008583261e06d7e26f48a9f277bde089ef4c
lenidar <55443074+lenidar@users.noreply.github.com>
2023-10-25 17:37:29 +0800

    Add project files.

---
+++ 2Y_OOPA_2324_ClassDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Y_OOPA_2324_ClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass1 sc1 = new SampleClass1();
+            Console.WriteLine(sc1.message());
+            sc1.changeTheMessage("This is the new message");
+            Console.WriteLine(sc1.message());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2Y_OOPA_2324_ClassDemo/SampleClass1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Y_OOPA_2324_ClassDemo
+{
+    internal class SampleClass1
+    {
+        private string theMessage = "This is the message";
+        
+        /// <summary>
+        /// This gets the message value of the class
+        /// </summary>
+        /// <returns>the message</returns>
+        public string message()
+        {
+            return theMessage;
+        }
+
+        /// <summary>
+        /// This sets the value of the message
+        /// </summary>
+        /// <param name="newMessage">The new message</param>
+        public void changeTheMessage(string newMessage)
+        {
+            theMessage = newMessage;
+        }
+        
+    }
+}
